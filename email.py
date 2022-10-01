aa66ebe4df0e7fbca1524dda30638aaa1cb4d7ce
jeophil <95517388+jeophil@users.noreply.github.com>
2022-10-01 15:54:17 +0530

    Add files via upload

---
+++ email.py
@@ -0,0 +1,6 @@
+email = input("Enter your email:").strip()
+username = email[:email.index("@")]
+domain_name = email[email.index("@")+1:]
+
+format = (f"Your username is '{username}'and your domain is '{domain_name}'")
+print(format)
